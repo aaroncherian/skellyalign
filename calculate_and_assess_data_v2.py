a74a4f3094fe579c62d7167993e450c9462271af
aaroncherian <aaron.cherian2@gmail.com>
2023-09-13 12:25:45 -0400

    adding save capabilities

--- calculate_and_assess_data_v2.py
+++ calculate_and_assess_data_v2.py
@@ -14,7 +14,7 @@ dataframe_of_3d_data = load_and_process_data(path_to_freemocap_data=freemocap_da
 
 error_metrics_dict = get_error_metrics(dataframe_of_3d_data=dataframe_of_3d_data)
 
-
-
+error_metrics_dict['absolute_error_dataframe'].to_csv(Path(r"D:\2023-05-17_MDN_NIH_data\1.0_recordings\calib_3\sesh_2023-05-17_14_53_48_MDN_NIH_Trial3\output_data\absolute_error_dataframe.csv"))
+error_metrics_dict['rmse_dataframe'].to_csv(Path(r"D:\2023-05-17_MDN_NIH_data\1.0_recordings\calib_3\sesh_2023-05-17_14_53_48_MDN_NIH_Trial3\output_data\rmse_dataframe.csv"))
 f = 2 
 
