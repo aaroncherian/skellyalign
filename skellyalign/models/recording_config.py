7b991dbdf07b5affd5a280c60f65d765ec2c1ff8
aaroncherian <aaron.cherian2@gmail.com>
2024-05-15 12:26:28 -0400

    Update recording_config.py

--- skellyalign/models/recording_config.py
+++ skellyalign/models/recording_config.py
@@ -13,7 +13,6 @@ class RecordingConfig(BaseModel):
     frames_to_sample: int = Field(20, gt=0, description="Number of frames to sample in each RANSAC iteration")
     max_iterations: int = Field(20, gt=0, description="Maximum number of RANSAC iterations")
     inlier_threshold: float = Field(50, gt=0, description="Inlier threshold for RANSAC")
-    frame_range: Optional[Tuple[int, Optional[int]]] = None
 
     @field_validator('path_to_recording', 'path_to_freemocap_output_data', 'path_to_qualisys_output_data', mode='after')
     def validate_paths(cls, v):
