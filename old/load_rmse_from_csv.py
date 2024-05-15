47f5402d7be2a80cf2ed7a32bfd93e4ef4d1f741
aaroncherian <aaron.cherian2@gmail.com>
2024-05-15 18:07:39 -0400

    delete old stuff

--- old/load_rmse_from_csv.py
+++
@@ -1,9 +0,0 @@
-
-
-import pandas as pd
-
-# Load the DataFrame
-csv_path = r"D:\2023-05-17_MDN_NIH_data\1.0_recordings\calib_3\sesh_2023-05-17_14_53_48_MDN_NIH_Trial3\output_data\rmse_dataframe.csv"
-df = pd.read_csv(csv_path).reset_index(drop=True)
-
-f = 2
