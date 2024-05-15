f9b4852df2011d015e4aaf85ddf5a3e4f32e9a2a
aaroncherian <aaron.cherian2@gmail.com>
2024-05-15 10:43:37 -0400

    Update sample_recording_config.py

--- sample_recording_config.py
+++ sample_recording_config.py
@@ -1,13 +1,16 @@
 from skellyalign.models.recording_config import RecordingConfig
+from MDN_validation_marker_set import markers_to_extract, qualisys_markers, mediapipe_markers
 
 
 sample_recording_config = RecordingConfig(
     path_to_recording="path/to/recording",
     path_to_freemocap_output_data="path/to/freemocap_output_data",
     path_to_qualisys_output_data="path/to/qualisys_output_data",
-    freemocap_
-    qualisys_marker_list=["marker1", "marker2"],
-    markers_to_compare_list=["marker1", "marker2"],
-    frame_for_comparison=0,
+    freemocap_markers=mediapipe_markers,
+    qualisys_markers=qualisys_markers,
+    markers_for_alignment=markers_to_extract,
+    frames_to_sample=20,
+    max_iterations=20,
+    inlier_threshold=50
 )
 
