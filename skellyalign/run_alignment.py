80dac06f34873cbc8ec06a794120207af7fcff2f
aaroncherian <aaron.cherian2@gmail.com>
2024-05-16 14:03:53 -0400

    Update run_alignment.py

--- skellyalign/run_alignment.py
+++ skellyalign/run_alignment.py
@@ -1,10 +1,9 @@
 from skellyalign.models.recording_config import RecordingConfig
 from skellyalign.utilities.data_handlers import DataLoader, DataProcessor
-import numpy as np
 from skellyalign.align_things import get_best_transformation_matrix_ransac, apply_transformation
 from skellyalign.plots.scatter_3d import plot_3d_scatter
 
-def main(recording_config:RecordingConfig):
+def run_ransac_alignment(recording_config:RecordingConfig):
 
     freemocap_dataloader = DataLoader(path=recording_config.path_to_freemocap_output_data)
     freemocap_data_processor = DataProcessor(data=freemocap_dataloader.data_3d, marker_list=recording_config.freemocap_markers, markers_for_alignment=recording_config.markers_for_alignment)
@@ -17,7 +16,8 @@ def main(recording_config:RecordingConfig):
     best_transformation_matrix = get_best_transformation_matrix_ransac(freemocap_data = freemocap_extracted, qualisys_data = qualisys_extracted, frames_to_sample=recording_config.frames_to_sample, max_iterations=recording_config.max_iterations, inlier_threshold=recording_config.inlier_threshold)
     aligned_freemocap_data = apply_transformation(best_transformation_matrix, freemocap_dataloader.data_3d)
     
-    plot_3d_scatter(freemocap_data=aligned_freemocap_data, qualisys_data=qualisys_dataloader.data_3d)
+    return aligned_freemocap_data
+    # plot_3d_scatter(freemocap_data=aligned_freemocap_data, qualisys_data=qualisys_dataloader.data_3d)
 
     f = 2   
 
