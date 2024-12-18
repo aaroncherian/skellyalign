d91e96bfa1c0f3bc352c6e4e36e0cca7fb4d8ff2
aaroncherian <aaron.cherian2@gmail.com>
2024-12-18 16:02:45 -0500

    repo cleanup

--- skellyalign/temporal_alignment/run_temporal_synchronization.py
+++ skellyalign/temporal_alignment/run_temporal_synchronization.py
@@ -1,17 +1,8 @@
 from skellyalign.temporal_alignment.create_temporal_sync_config import create_temporal_sync_config
-
-from skellyalign.temporal_alignment.qualisys_data_processing.process_qualisys_tsv import TSVProcessor, JointCenterCalculator, get_unix_start_time
-from skellyalign.temporal_alignment.freemocap_data_processing import create_freemocap_unix_timestamps
-from skellyalign.temporal_alignment.qualisys_data_processing.resample_qualisys_data import QualisysResampler
-from skellyalign.temporal_alignment.run_skellyforge_rotation import run_skellyforge_rotation
-from skellyalign.temporal_alignment.configs.temporal_alignment_config import LagCorrectionSystemComponent, LagCorrector
-
 from skellyalign.temporal_alignment.configs.recording_config import Recording
 
-import numpy as np
 
 from typing import List
-
 from skellyalign.temporal_alignment.temporal_synchronizer import TemporalSyncManager
 
 
@@ -30,7 +21,7 @@ def run_temporal_synchronization(recording_config: Recording):
 
 if __name__ == '__main__':
     from skellyalign.temporal_alignment.create_temporal_sync_config import create_temporal_sync_config
-    from skellyalign.temporal_alignment.qualisys_data_processing.joint_center_weights.full_body_joint_center_weights import joint_center_weights
+    from skellyalign.temporal_alignment.qualisys_processing.joint_center_weights.full_body_joint_center_weights import joint_center_weights
 
     def setup_recording_config(path: str) -> Recording:
         config = create_temporal_sync_config(path)
@@ -44,7 +35,7 @@ if __name__ == '__main__':
         )
         return config
     
-    recording_folder_path= r"D:\2024-10-30_treadmill_pilot\processed_data\sesh_2024-10-30_15_45_14_mdn_gait_7_exposure"
+    recording_folder_path= r"D:\2024-04-25_P01\1.0_recordings\sesh_2024-04-25_15_44_19_P01_WalkRun_Trial1"
     
     recording_config = setup_recording_config(recording_folder_path)
 
